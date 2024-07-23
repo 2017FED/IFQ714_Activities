e33a44517a2b7416194769255603b9a2e0691977
2017FED <sainiparminder2015@gmail.com>
2024-07-23 12:02:44 +1000

    Class activities week 1

---
+++ greetings.js
@@ -0,0 +1,16 @@
+function createGreeting(name) {
+  // TODO: Create a new variable named "greeting"
+  // Assign to it the string "Hi! It's great to meet you, "
+  let greeting = "Hi! It's great to meet you, ";
+
+  return greeting + name;
+}
+
+// TODO: Create a new variable called "name"
+// Assign to it the string "Tim", or alternatively your name
+const name = "Parminder";
+
+const message = createGreeting(name);
+
+// Show the message in the console.
+console.log(message);
---
+++ shoppingCart.js
@@ -0,0 +1,27 @@
+const applePrice = 2.5;
+const appleQuantity = 2;
+
+const orangePrice = 1.54;
+const orangeQuantity = 4;
+
+// TODO: Make up a price and quantity for your favourite fruit
+
+const bananaPrice = 2.3;
+const bananaQuantity = 2;
+
+const totalCostApple = applePrice * appleQuantity;
+
+// TODO: Calculcate the total cost for oranges and your favourite fruit
+const totalCostOrange = orangePrice * orangeQuantity;
+const totalCostBanana = bananaPrice * bananaQuantity;
+
+// TODO: Calculate the overall total cost
+const overallTotalCost = totalCostApple + totalCostOrange + totalCostBanana;
+
+// Display the individual and overall total costs
+console.log(`Total cost for apples: $${totalCostApple}`);
+console.log(`Total cost for oranges: $${totalCostOrange}`);
+
+// TODO: Log out the cost for your favourite fruit
+console.log(`Total cost for bananas: $${totalCostBanana}`);
+console.log(`Overall total cost: $${overallTotalCost}`);
